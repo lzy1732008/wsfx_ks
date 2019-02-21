bfb44e6cec0ce741b53a724de20c948efd39ac48
lzy1732008 <lzy1732008@163.com>
2019-02-21 10:54:59 +0800

    update_20190221 add sgls_model.py

--- wsfx2/code/train/run.py
+++ wsfx2/code/train/run.py
@@ -12,7 +12,7 @@ from sklearn import metrics
 import tensorflow.contrib.keras as kr
 
 
-from wsfx2.code.models.ce_initgate import modelConfig,CNN
+from wsfx2.code.models.sgls_model import modelConfig,CNN
 from wsfx2.code.train.loader import batch_iter,batch_iter_test,data_load
 
 data_dir = '../../source/dataset/set_4'
@@ -41,11 +41,11 @@ lastksinfo = 'Fasle' #defalut is true
 singleuse = '3'
 relu = 'False' #defalut is true
 
-save_dir  = '../../result/set4/ce_initgate'  #修改处
+save_dir  = '../../result/set4/sgls_model'  #修改处
 # save_path = save_dir+'/checkpoints/precessF:'+str(precessF)+'-MirrorGate:'+str(mirrorgate)+ks_order+'-time:'+str(times)+'noaddks-30-30-'+str(n_number)+'gram-gate'+str(gate_n)+'-'+str(reg)+'/best_validation'  # 最佳验证结果保存路径
 # tensorboard_dir = save_dir+'/tensorboard/precessF:'+str(precessF)+'-MirrorGate:'+str(mirrorgate)+ks_order+'-time:'+str(times)+'noaddks-30-30-'+str(n_number)+'gram-gate'+str(gate_n)+'-'+str(reg)  #修改处
-ckpath = 'times:2'
-tbpath = 'times:2'
+ckpath = 'times:1'
+tbpath = 'times:1'
 save_path = save_dir+'/checkpoints/'+ckpath+'/best_validation'
 tensorboard_dir = save_dir + '/tensorboard/' + tbpath
 
@@ -324,10 +324,10 @@ def test():
     print("Time usage:", time_dif)
     return y_test_cls,y_pred_cls
 
-#train()
+train()
 
-y_test_cls, y_pred_cls= test()
-wsnamels = getwslist(model)
-wsevaluate(y_test_cls, y_pred_cls,wsnamels)
+# y_test_cls, y_pred_cls= test()
+# wsnamels = getwslist(model)
+# wsevaluate(y_test_cls, y_pred_cls,wsnamels)
 
 
